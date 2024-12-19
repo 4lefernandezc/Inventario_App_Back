e924eb172287292a29090e79429bfef53b6fe809
sebastianDLL <delgadillosebastian75@gmail.com>
2024-12-19 17:52:20 -0400

    Agregar archivo de configuración de entorno y mejorar la configuración de la API

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,37 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.useGlobalPipes(new ValidationPipe());
+  app.setGlobalPrefix('api');
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: '1',
+  });
+  app.enableCors();
+
+  const config = new DocumentBuilder()
+    .setTitle('API Comercial Franco')
+    .setDescription(
+      'API Rest Sistema de Ventas e Inventario Comercial Franco',
+    )
+    .setVersion('1.0')
+    // .addTag('') // Agregar etiquetas
+    // .addBearerAuth({
+    //   type: 'http',
+    //   scheme: 'bearer',
+    //   bearerFormat: 'JWT',
+    //   in: 'header',
+    // })
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('apidoc', app, document);
+
+  await app.listen(process.env.PORT);
+  console.log(`Servidor esta corriendo en ${await app.getUrl()}/apidoc`);
 }
 bootstrap();
