2da0a507b84943e1a67b476dc147507afba44c21
sebastianDLL <delgadillosebastian75@gmail.com>
2025-01-17 18:03:41 -0400

    Agregar middleware de limitación de tasa para el inicio de sesión.

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { ConfigModule } from '@nestjs/config';
@@ -6,6 +6,7 @@ import { UsuariosModule } from 'src/usuarios/usuarios.module';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
 import { JwtStrategy } from './strategies/jwt.strategy';
+import { LoginRateLimitMiddleware } from 'src/middlewares/login-rate-limit.middleware';
 
 @Module({
   imports: [
@@ -16,5 +17,14 @@ import { JwtStrategy } from './strategies/jwt.strategy';
   ],
   controllers: [AuthController],
   providers: [AuthService, JwtStrategy],
+
+  
 })
-export class AuthModule {}
+
+export class AuthModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+    .apply(LoginRateLimitMiddleware) // Aplica el middleware en el controlador completo
+    .forRoutes(AuthController); // Aplica en todas las rutas del controlador de autenticación
+  }
+}
